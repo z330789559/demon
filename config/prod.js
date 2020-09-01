520b9b27ea6248e3d340dd3d635ac4e4a46f7f1c
libaozhong <libaozhong@xforceplus.com>
2020-09-01 22:16:16 +0800

    打包

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,11 @@
 const isH5 = process.env.CLIENT_ENV === 'h5'
 
-const HOST = '"http://localhost:3721"'
+const HOST = '""'
 const HOST_M = '""'
 
 // XXX 搭了个 proxy 用于演示 prod 环境的 H5
-const HOST_H5 = '"http://localhost:3721"'
-const HOST_M_H5 = '"http://localhost:3721"'
+const HOST_H5 = '""'
+const HOST_M_H5 = '""'
 
 module.exports = {
   env: {
