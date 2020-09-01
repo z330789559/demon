4e620de9093bd9e6ada259783ef04610af32eb40
libaozhong <libaozhong@xforceplus.com>
2020-09-01 22:41:00 +0800

    repaire path

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,32 +1,32 @@
 // /* eslint-disable */
-export const host = "/"
-// export const hostM = HOST_M
+export const host_prd_prd = "http://house.yffrom.com:3721"
+// export const host_prdM = host_prd_M
 // /* eslint-enable */
-// export const app_host= APP_HOST
+// export const app_host_prd= APP_host_prd
 // pic
 
 
-export const API_MAIN_CONTENT=`${host}/api/main/content`
-export const API_MAIN_RECOMMEND=`${host}/api/main/recommend`
+export const API_MAIN_CONTENT=`${host_prd}/api/main/content`
+export const API_MAIN_RECOMMEND=`${host_prd}/api/main/recommend`
 
 
 
-export const API_CREATE_LIST=`${host}/api/create/list`
-export const API_CREATE_CATE=`${host}/api/create/cate`
+export const API_CREATE_LIST=`${host_prd}/api/create/list`
+export const API_CREATE_CATE=`${host_prd}/api/create/cate`
 
 
-export const API_FORM_LIST_LOAD=`${host}/api/form/list`
-export const API_FORM_SUBCATE_LOAD=`${host}/api/form/subCate`
+export const API_FORM_LIST_LOAD=`${host_prd}/api/form/list`
+export const API_FORM_SUBCATE_LOAD=`${host_prd}/api/form/subCate`
 
-export const API_FORM_ElEMENT_LOAD=`${host}/api/form/elements`
+export const API_FORM_ElEMENT_LOAD=`${host_prd}/api/form/elements`
 
 
-export const API_SENCE_ElEMENT_LOAD=`${host}/api/sences`
-export const API_OPUS_LOAD_PAGE=`${host}/api/opus`
-export const API_OPUS_LOAD_LIST=`${host}/api/opus/list`
-export const API_SENCE_DETAIL_LOAD=`${host}/api/opus/detail`
+export const API_SENCE_ElEMENT_LOAD=`${host_prd}/api/sences`
+export const API_OPUS_LOAD_PAGE=`${host_prd}/api/opus`
+export const API_OPUS_LOAD_LIST=`${host_prd}/api/opus/list`
+export const API_SENCE_DETAIL_LOAD=`${host_prd}/api/opus/detail`
 
 // user
-export const API_USER = `${host}/xhr/user/getDetail.json`
-export const API_USER_LOGIN = `${host}/xhr/u/mailLogin.json`
-export const API_CHECK_LOGIN = `${host}/xhr/u/checkLogin.json`
+export const API_USER = `${host_prd}/xhr/user/getDetail.json`
+export const API_USER_LOGIN = `${host_prd}/xhr/u/mailLogin.json`
+export const API_CHECK_LOGIN = `${host_prd}/xhr/u/checkLogin.json`
